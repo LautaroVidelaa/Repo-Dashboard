34947267dc7f7f635aa613246b5bebbaad3d71bc
Enzo Rojas <enzo.e.rojas@gmail.com>
2026-04-27 08:49:45 -0300

    add navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h2 className="logo">App de jugadores</h2>
+    </nav>
+  );
+}
+
+export default Navbar;
