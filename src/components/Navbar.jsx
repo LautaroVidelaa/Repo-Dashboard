b75c5ce945c6d120ddea8444fc8a6f4d5693048c
Enzo Rojas <enzo.e.rojas@gmail.com>
2026-04-27 10:27:48 -0300

    add material ui

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 import "./Navbar.css";
 
 function Navbar() {
   return (
-    <nav className="navbar">
-      <h2 className="logo">App de jugadores</h2>
-    </nav>
+    <AppBar position="static" className="navbar">
+      <Toolbar className="toolbar">
+        <Typography variant="h6" className="logo">
+          App de jugadores
+        </Typography>
+      </Toolbar>
+    </AppBar>
   );
 }
 
